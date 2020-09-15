1a42b887a464c3ea41eb5f0948b279bc732831e4
Cecilie Nielsen <cph-cn274@cphbusiness.dk>
2020-09-15 20:57:13 +0200

    updated jokes page

--- src/main/webapp/jokeFunctionality.js
+++ src/main/webapp/jokeFunctionality.js
@@ -1,40 +1,35 @@
 //OPGAVE 3 - JOKES
 
 function createJokesTable(data) {
-    const headers = "<table class=\"table\"><tr><th>Joke ID</th><th>Jokes</th></tr>";
+    console.log(data);
+    const headers = "<table class=\"table\"><tr><th>Joke</th><th>Type</th><th>Reference</th></tr>";
 
-    let mappedRows = data.map(joke => "<tr><td>" + joke.id + "</td><td>" + joke.joke + "</td></tr>");
+    let mappedRows = data.map(joke => "<tr><td>" + joke.the_joke + "</td><td>" + joke.type + "</td><td>" + joke.reference + "</td></tr>");
     let joinedRows = mappedRows.join("");
     document.getElementById("jokeTable").innerHTML = headers + joinedRows + "</table>";
 
 }
 
 function fetchJokes() {
-    const url = "api/joke/all"; //TODO: Change
-    event.preventDefault() 
+    const url = "api/joke/all";
     fetch(url)
         .then(res => res.json())
         .then(data => createJokesTable(data));
-    
 }
 
  function fetchJoke() {
     const jokeID = document.getElementById("inputId").value;
-    const url = "api/joke/"; //TODO: Change
-    event.preventDefault() 
-    fetch(url + jokeID)
-        .then(res => res.json())
-        .then(data => createJokesTable(data));
-
-    
-}
-function fetchRandomJoke() {
-    const url = "api/joke/random"; //TODO: Change
-    event.preventDefault() 
+    const url = "api/joke/" + jokeID;
     fetch(url)
         .then(res => res.json())
-        .then(data => createJokesTable(data));
-    
+        .then(data => createJokesTable([data]));    
+}
+
+function fetchRandomJoke() {
+    const url = "api/joke/random";
+    fetch(url)
+        .then(res => res.json())
+        .then(data => createJokesTable([data]));
 }
 
 fetchJokes();
