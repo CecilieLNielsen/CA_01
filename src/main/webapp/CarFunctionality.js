f4e1dcaf70247ef328e77ab43bce3fd9666d06e8
Abed01-lab <abedharirii@gmail.com>
2020-09-20 00:35:29 +0200

    fixed car

--- src/main/webapp/CarFunctionality.js
+++ src/main/webapp/CarFunctionality.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-getCarsFilteredByYear();
+getCars();
 
 function createCarTable(data) {
     console.log(data);
