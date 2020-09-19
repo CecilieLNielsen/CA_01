82d8889834a554f325d2eb860c6f71a5e87afacc
rh <cph-178rh@cphbusiness.dk>
2020-09-19 20:50:48 +0200

    Added my part to status page and finished jokes endpoint test. Added getJokebyType endpoint and tests. Added test of error msg if get by type fails

--- src/main/java/rest/JokeResource.java
+++ src/main/java/rest/JokeResource.java
@@ -21,6 +21,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import utils.EMF_Creator;
 
 /**
@@ -75,6 +76,20 @@ public class JokeResource {
     public String getRandom() {
         return GSON.toJson(FACADE.getRandom());
     }
+    
+    @GET
+    @Path("/bytype/{type}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getByType(@PathParam("type") String type) {
+        
+        List<JokeDTO> jdto = FACADE.getByType(type);
+        if(jdto == null || jdto.isEmpty()){
+            return Response.status(404).entity("{\"Ups\": 404, \"msg\":\"We dont have that kind of jokes\"}").build();
+        }
+        return Response.ok(GSON.toJson(jdto)).build();
+    }
+    
+    
     
     
 
--- src/test/java/rest/JokeResourceTest.java
+++ src/test/java/rest/JokeResourceTest.java
@@ -13,6 +13,8 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.util.HttpStatus;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
 import org.junit.jupiter.api.AfterAll;
@@ -28,18 +30,22 @@ import utils.EMF_Creator;
  * @author hassanainali
  */
 public class JokeResourceTest {
-    
+
     private static final int SERVER_PORT = 7777;
     private static final String SERVER_URL = "http://localhost/api";
     static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
     private static HttpServer httpServer;
     private static EntityManagerFactory emf;
 
+    private Joke j1;
+    private Joke j2;
+    private Joke j3;
+
     static HttpServer startServer() {
         ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
         return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
     }
-    
+
     @BeforeAll
     public static void setUpClass() {
         // This method must be called before you request the EntityManagerFactory
@@ -48,9 +54,10 @@ public class JokeResourceTest {
 
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
-        em.createQuery("DELETE FROM Joke j").executeUpdate();
+        em.createQuery("DELETE FROM Joke").executeUpdate();
         em.createNativeQuery("ALTER TABLE `JOKE` AUTO_INCREMENT = 1").executeUpdate();
         em.getTransaction().commit();
+        em.close();
 
         httpServer = startServer();
 
@@ -59,7 +66,7 @@ public class JokeResourceTest {
         RestAssured.port = SERVER_PORT;
         RestAssured.defaultParser = Parser.JSON;
     }
-    
+
     @AfterAll
     public static void closeTestServer() {
         // System.in.read();
@@ -67,31 +74,38 @@ public class JokeResourceTest {
         EMF_Creator.endREST_TestWithDB();
         httpServer.shutdownNow();
     }
-    
+
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         EntityManager em = emf.createEntityManager();
+        j1 = new Joke("I hate Russian dolls. They're so full of themselves.", "Bad", "Russia");
+        j2 = new Joke("What do you tell actors to break a leg? Because every play has a cast!", "Bad", "Actors");
+        j3 = new Joke("What do you call a bus full of lawyers going over a cliff? A good start", "Good", "Law");
         try {
             em.getTransaction().begin();
             em.createQuery("DELETE from Joke").executeUpdate();
-            em.persist(new Joke("I hate Russian dolls. They're so full of themselves.", "Bad", "Russia"));
-            em.persist(new Joke("What do you tell actors to break a leg? Because every play has a cast!", "Bad", "Actors"));
+
+            em.persist(j1);
+            em.persist(j2);
+            em.persist(j3);
+
             em.getTransaction().commit();
         } finally {
             em.close();
         }
     }
-    
+
     @AfterEach
     public void tearDown() {
         // Remove any data after each test was run
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
         em.createQuery("DELETE FROM Joke j").executeUpdate();
-        em.createNativeQuery("ALTER TABLE `JOKE` AUTO_INCREMENT = 1").executeUpdate();
+        // em.createNativeQuery("ALTER TABLE `JOKE` AUTO_INCREMENT = 1").executeUpdate();
         em.getTransaction().commit();
+        em.close();
     }
-    
+
     @Test
     public void testGetAll() {
         given()
@@ -100,24 +114,54 @@ public class JokeResourceTest {
                 .then()
                 .assertThat()
                 .statusCode(HttpStatus.OK_200.getStatusCode())
-                .body("jokeInformation", hasSize(2));
+                .body("jokeInformation", hasSize(3))
+                .body("the_joke", hasItem("I hate Russian dolls. They're so full of themselves."));
     }
-    /*
+
     @Test
-    public void testGetJokeByID(){
-        
+    public void testGetJokeByID() {
+        given()
+                .contentType("application/json")
+                .get("/joke/{id}", j3.getId())
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("the_joke", equalTo("What do you call a bus full of lawyers going over a cliff? A good start"));
+
     }
-    */
-    /*
+
     @Test
-    public void testGetRandom(){
+    public void testGetRandom() {
         given()
                 .contentType("application/json")
                 .get("/joke/random")
                 .then()
                 .assertThat()
-                .statusCode(HttpStatus.OK_200.getStatusCode())
-                .body()
+                .statusCode(HttpStatus.OK_200.getStatusCode());
     }
-    */
+
+    @Test
+    
+    public void testGetByType() {
+        given().
+                contentType("application/json")
+                .get("/joke/bytype/{type}", j2.getType())
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("jokeInformation", hasSize(2))
+                .body("type", hasItem("Bad"));
+    }
+    
+    @Test 
+    public void testGetByTypeError() {
+                given().
+                contentType("application/json")
+                .get("/joke/bytype/xxxxx")
+                .then()
+                .assertThat()
+                .statusCode(404)
+                .body("msg", equalTo("We dont have that kind of jokes"));
+    }
+
 }
